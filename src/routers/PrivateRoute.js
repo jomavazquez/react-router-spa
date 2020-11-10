df5b4412c9a78f6a36b09e58c1692cd2751df7e7
José María Vázquez <jomavazquez@yahoo.es>
2020-11-10 18:49:39 +0100

    Fix

--- src/routers/PrivateRoute.js
+++ src/routers/PrivateRoute.js
@@ -17,7 +17,7 @@ export const PrivateRoute = ({
                 
                 ( isAuthenticated )
                 ? <Component {...props} />
-                : <Redirect to="/login" />
+                : <Redirect to="https://jomavazquez.github.io/react-router-spa/login" />
             )}
         />
 
